4977037798955fa92059e0fc3da2b406f28b57fc
Chai <ztl44582@gmail.com>
2026-05-16 11:58:24 +0800

    fixx

--- script_train_vae3d_kl8.sh
+++ script_train_vae3d_kl8.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-OUT_DIR='output/vae3d'
+OUT_DIR='output/vae3d_short'
 CFG_FILE='config/vae_kl_ft2_fs8_z16.yaml'
 RESUME_DIR="${OUT_DIR}"
 
@@ -10,11 +10,14 @@ mkdir -p "${OUT_DIR}"
 PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
 CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nproc_per_node=4 main_vae.py \
   --batch_size 2 \
-  --epochs 100 \
+  --epochs 5 \
   --lr 4e-4 \
   --weight_decay 0.01 \
   --disc_loss_scale 0.1 \
   --save_last_freq 1 \
+  --eval_freq 1 \
+  --limit_num 3 \
+  --online_eval \
   --persistent_workers \
   --no_amp \
   --resume "${RESUME_DIR}" \
